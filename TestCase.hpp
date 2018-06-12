5be166e2f735aa3ecdbf7e0efdb0aa4f92cd585c
BarZamsky <barza@DESKTOP-GN3U8LK.(none)>
2018-06-12 14:47:21 +0300

    fix bug

--- TestCase.hpp
+++ TestCase.hpp
@@ -8,19 +8,20 @@ class TestCase
 {
     string _testName;
     ostream& stream;
-    int pass,fail;
+    int pass,fail,total;
 
     public:
-    TestCase(const string s, ostream& os): _testName(s), stream(os) { pass=0;fail=0;}
+    TestCase(const string s, ostream& os): _testName(s), stream(os) { pass=0;fail=0;total=0;}
                                         
     template <typename T>
     TestCase check_equal(T a, T b)
     {
+    total++;
     if (a==b) pass++;
     else
     {        
         fail++;
-        stream<<_testName<<": Failure in test #"<< fail <<": "<<a<<" should equal "<<b<<"!\n";
+        stream<<_testName<<": Failure in test #"<< total <<": "<<a<<" should equal "<<b<<"!\n";
     }
     return *this; 
     }
@@ -28,24 +29,25 @@ class TestCase
      template <typename T>
     TestCase check_different(T a, T b)
     {
-         if (a!=b) pass++;
+    total++;
+    if (a!=b) pass++;
     else
     {        
         fail++;
-        stream<<_testName<<": Failure in test #"<< fail <<": "<<a<<" should not equal "<<b<<"!\n";
+        stream<<_testName<<": Failure in test #"<< total <<": "<<a<<" should not equal "<<b<<"!\n";
     }
     return *this; 
-
     }
 
    template <typename T1,typename T2,typename T3>
    TestCase check_function(T1 (f) ,T2 num, T3 result)
    {
+       total++;
         if (f(num)==result){ pass++;}
         else
         {
             fail++;
-            stream<<_testName<<": Failure in test #"<< fail <<": Function should return "<<result<<" but returned "<<f(num)<<"\n";
+            stream<<_testName<<": Failure in test #"<< total <<": Function should return "<<result<<" but returned "<<f(num)<<"\n";
         }
         return *this;
    }
@@ -53,19 +55,20 @@ class TestCase
     template <typename T>
     TestCase check_output(T a, string str )
     {   
+        total++;
         stringstream ss;
         ss << a;
         if(ss.str().compare(str)==0) pass++;
         else {
             fail++;
-            stream<<_testName<<": Failure in test #"<< fail <<": string value should be "<< str << " but is " << ss.str() << "\n"; 
+            stream<<_testName<<": Failure in test #"<< total <<": string value should be "<< str << " but is " << ss.str() << "\n"; 
             }
             return *this;
     }
 
     void print()
     {
-        stream<< _testName<<": "<< fail<<" failed, "<<pass<<" passed, "<<fail+pass<<" total\n";
+        stream<< _testName<<": "<< fail<<" failed, "<<pass<<" passed, "<<total<<" total\n";
         stream<<"------\n";
     }
    
